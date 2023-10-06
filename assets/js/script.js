e48ba1a2a2aed28b1f591bdad4740cf028ddf46b
Vigneswari Sambandam <vigneswari@Vigneswaris-MacBook-Air.local>
2023-10-06 16:46:39 -0400

    Added functions to get the inputs from HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,41 @@
 var generateBtn = document.querySelector("#generate");
 var criteriaDiv = document.querySelector("#criteriaDiv");
 var passLength = document.getElementsByName("passLength");
+var passCriteria = document.getElementsByName("passCriteria");
 
 var pLen;
+var pCriteria = [];
 
 // Show the Password Criteria block for user input
 function showCriteria() {
   criteriaDiv.style.display = "block";
 }
 
+// Get the Length of Password 
 function getPasswordLength() { 
   for (i = 0; i < passLength.length; i++) {
     if (passLength[i].checked) {
       pLen = passLength[i].value;
-      console.log("Selected Password Length==", pLen);
     }      
   }
   return pLen;
 }
 
+// Get the Criteria of Password 
+function getPasswordCriteria() { 
+  for (i = 0; i < passCriteria.length; i++) {
+    if (passCriteria[i].checked) {
+      pCriteria.push(passCriteria[i].value);
+    }      
+  }
+  return pCriteria;
+}
+
 function generatePassword() { 
   pLen = getPasswordLength();
+  console.log("Selected Password Length==", pLen);
+  pCriteria = getPasswordCriteria();
+  console.log("Selected Criteris==", pCriteria);
 }
 
 // Write password to the #password input
@@ -37,4 +52,4 @@ function writePassword() {
 
 
 generateBtn.addEventListener("click", showCriteria);
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
