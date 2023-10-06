c451dff5c327aa38b784e16e08caf88809008c82
Vigneswari Sambandam <vigneswari@Vigneswaris-MacBook-Air.local>
2023-10-06 18:09:36 -0400

    Added function to Generate Password

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ var passCriteria = document.getElementsByName("passCriteria");
 var pLen;
 var pCriteria = [];
 
+// Line 11 to 14 defines the criteria requirements
+var lowerCharset = "abcdefghijklmnopqrstuvwxyz";
+var upperCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numCharset = "0123456789";
+var splCharset = "@#$&_";
+var selectedCharset = "";
+var generatedString = "";
+
 // Show the Password Criteria block for user input
 function showCriteria() {
   criteriaDiv.style.display = "block";
@@ -33,10 +41,51 @@ function getPasswordCriteria() {
 }
 
 function generatePassword() { 
-  pLen = getPasswordLength();
-  console.log("Selected Password Length==", pLen);
+  pLen = parseInt(getPasswordLength());
+  //console.log("Selected Password Length==", pLen);
   pCriteria = getPasswordCriteria();
-  console.log("Selected Criteris==", pCriteria);
+  //console.log("Selected Criteris==", pCriteria);
+
+  if (pLen === NaN) {
+    window.alert("Please choose the password length");
+  }
+  else if (pCriteria.length === 0) {
+    window.alert("Please choose the password criteria");
+  }
+  else { 
+    for (i = 0; i < pCriteria.length; i++) { 
+      if (pCriteria[i] === "uppercase") {
+        selectedCharset = selectedCharset + upperCharset;
+      }
+      if (pCriteria[i] === "lowercase") {
+        selectedCharset = selectedCharset + lowerCharset;
+      }
+      if (pCriteria[i] === "numeric") {
+        selectedCharset = selectedCharset + numCharset;
+      }
+      if (pCriteria[i] === "special") { 
+        selectedCharset = selectedCharset + splCharset;
+      }
+    }
+
+    // This function generates the new length for the password, where MIN value is from user input and MAX value is fixed to 128
+    var newLen = getRandomArbitrary(parseInt(getPasswordLength()), 128);
+
+  //  console.log("New length=="+newLen);
+  //  console.log(selectedCharset);
+  for (var i = 0; i < newLen; i++) { 
+    generatedString = generatedString + selectedCharset[Math.floor(Math.random() * selectedCharset.length)];
+  }
+  //console.log("Generated String=="+ generatedString);
+  }
+  
+  return generatedString;
+}
+
+
+//This function is retrieved from MDN site - Refer READ.ME file
+function getRandomArbitrary(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
 }
 
 // Write password to the #password input
@@ -49,7 +98,5 @@ function writePassword() {
 
 
 // Add event listener to generate button
-
-
 generateBtn.addEventListener("click", showCriteria);
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
